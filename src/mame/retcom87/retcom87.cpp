9bf8f7f401b7fba262ad718c7679458f20bd0fe9
Ming Zhang <w98zhang@gmail.com>
2024-04-12 15:14:03 -0700

    reference display by device instead of tag. some cleanup and comments.

--- src/mame/retcom87/retcom87.cpp
+++ src/mame/retcom87/retcom87.cpp
@@ -13,8 +13,9 @@ namespace
     gtvip_state(const machine_config &mconfig, device_type type, const char *tag)
         : driver_device(mconfig, type, tag),
           m_maincpu(*this, "maincpu"),
-          m_ymsnd(*this, "ym2149")
-          // m_mainmemory(*this, "main_ram", 0x8000, ENDIANNESS_LITTLE)
+          m_ymsnd(*this, "ym2149"),
+          m_vdp(*this, "tms9918")
+    // m_mainmemory(*this, "main_ram", 0x8000, ENDIANNESS_LITTLE)
     // m_mainmemory(*this, "main_ram", 0x10000, ENDIANNESS_LITTLE)
     {
     }
@@ -25,6 +26,7 @@ namespace
   private:
     required_device<g65816_device> m_maincpu;
     required_device<ym2149_device> m_ymsnd;
+    required_device<tms9918_device> m_vdp;
 
     // memory_share_creator<u8> m_mainmemory;
 
@@ -42,45 +44,57 @@ namespace
     // m_ymsnd->port_a_write_callback().set(FUNC(st_state::psg_pa_w));
     // m_ymsnd->port_b_write_callback().set("cent_data_out", FUNC(output_latch_device::write));
 
-    tms9928a_device &vdp(TMS9928A(config, "tms9928a", XTAL(10'738'635)));
-    vdp.set_screen("screen");
-    vdp.set_vram_size(0x4000);
+    // referenced colecovision which uses TMS9928A: src/mame/coleco/coleco.cpp
+    // 10.738633MHz clock frequency and 16K vram
+    // not sure about the SCREEN part but just copied it over
+    TMS9918(config, m_vdp, XTAL(10'738'635));
+    m_vdp->set_screen("screen");
+    m_vdp->set_vram_size(0x4000);
     SCREEN(config, "screen", SCREEN_TYPE_RASTER);
   }
 
+  // see MAME docs on memory: https://docs.mamedev.org/techspecs/memory.html
   void gtvip_state::main_memmap(address_map &map)
   {
+    // 32kB SRAM
+    map(0x0000, 0x7FFF).ram();
 
-    map(0x0000, 0x1fff).rom();
-    // map(0x6000, 0x63ff).ram().mirror(0x1c00);
-    // map(0x8000, 0xffff).rom();
-  
+    // 32kB flash memory
+    // some of this range is used for I/O (below)
+    map(0x8000, 0xffff).rom();
 
-  // Add memory map here
-  // map(0x0000, 0x1fff).rom();
-  // map(0x0, 0x7FFF).share("main_ram");
-  // map(0x8000, 0xFFFF).rom().region("maincpu", 0x0000);
-  // map(0x8000, 0xFFFF).rom();
+    // I/O
 
-  // from colecovision
-  // map(0xff0000, 0xff0000).rw("ym2149", FUNC(ay8910_device::data_r), FUNC(ay8910_device::address_w)).mirror(0xfc);
-  // map(0xff0002, 0xff0002).rw("ym2149", FUNC(ay8910_device::data_r), FUNC(ay8910_device::data_w)).mirror(0xfc);
-  // map(0xa0, 0xa1).mirror(0x1e).rw("tms9928a", FUNC(tms9928a_device::read), FUNC(tms9928a_device::write));
+    // display
+    // DFC0: TMS9118 VRAM Access (aliased to DFC2, DFC4, DFC6) (not $C000 like in datasheet)
+    // DFC1: TMS9118 Register Access (aliased to DFC3, DFC5, DFC7) (not $C002 like in datasheet)
+    // referenced Tomy Tutor(?)g: src/mame/tomy/tutor.cpp
+    // but referencing it as a device instead of tag for consistency
+    map(0xdfc0, 0xdfc0).rw(m_vdp, FUNC(tms9918_device::vram_read), FUNC(tms9918_device::vram_write));         /*VDP data*/
+    map(0xdfc1, 0xdfc1).rw(m_vdp, FUNC(tms9918_device::register_read), FUNC(tms9918_device::register_write)); /*VDP status*/
 
-  map(0xdfc0, 0xdfc0).rw("tms9928a", FUNC(tms9928a_device::vram_read), FUNC(tms9928a_device::vram_write));         /*VDP data*/
-  map(0xdfc1, 0xdfc1).rw("tms9928a", FUNC(tms9928a_device::register_read), FUNC(tms9928a_device::register_write)); /*VDP status*/
+    // sound
+    // DF10: Data Send for YM2149 soundchip #1
+    // DF11: Register Select for YM2149 soundchip #1
+    // DF12: Data Send for YM2149 soundchip #2
+    // DF13: Register Select for YM2149 soundchip #2
 
-  map(0x8000, 0xffff).rom();
-}
+    // controllers
+    // DF00-DF03: Controller 1 through 4 inputs (aliased to DF04-DF07)
+    // When Controller Select Pin output (P51, pin 4, J4-P5x connector) is 1: [C B C B Right Left Down Up]
+    // When Controller Select Pin output (P51, pin 4, J4-P5x connector) is 0: [Start A Start A 0 0 Down Up]
+    
 
-INPUT_PORTS_START(gtvip_inputs)
+  }
 
-INPUT_PORTS_END
+  INPUT_PORTS_START(gtvip_inputs)
 
-ROM_START(gtvip)
-ROM_REGION(0x10000, "maincpu", 0)
-ROM_LOAD("textdemo.bin", 0x8000, 0x8000, CRC(4cf363dc) SHA1(bed707ec2ebb3e6cddfc6db58d78e436af05961a))
-ROM_END
+  INPUT_PORTS_END
+
+  ROM_START(gtvip)
+  ROM_REGION(0x10000, "maincpu", 0)
+  ROM_LOAD("textdemo.bin", 0x8000, 0x8000, CRC(4cf363dc) SHA1(bed707ec2ebb3e6cddfc6db58d78e436af05961a))
+  ROM_END
 
 } // namespace
 
