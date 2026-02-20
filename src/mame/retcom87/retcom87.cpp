b062033a771c722d279b2e0fd5ae4e8c8f9b021e
cheesecake <chi.xuke.ki@gmail.com>
2026-02-19 22:10:46 -0500

    Cleanup

--- src/mame/retcom87/retcom87.cpp
+++ src/mame/retcom87/retcom87.cpp
@@ -5,8 +5,6 @@
 #include "sound/ay8910.h"
 #include "video/tms9928a.h"
 
-// #include "machine/rescap.h"  // for set_resistors_load
-
 #include "speaker.h"
 
 // Unlike other W65C816 variants, the W65C265 does *not* start in emulation mode
@@ -35,13 +33,11 @@ namespace
   {
   public:
     gtvip_state(const machine_config &mconfig, device_type type, const char *tag)
-        : driver_device(mconfig, type, tag),
-          m_maincpu(*this, "maincpu"),
-          m_ymsnd_0(*this, "ym2149_0"),
-          m_ymsnd_1(*this, "ym2149_1"),
-          m_vdp(*this, "tms9918")
-    // m_mainmemory(*this, "main_ram", 0x8000, ENDIANNESS_LITTLE)
-    // m_mainmemory(*this, "main_ram", 0x10000, ENDIANNESS_LITTLE)
+        : driver_device(mconfig, type, tag)
+        , m_maincpu(*this, "maincpu")
+        , m_ymsnd_0(*this, "ym2149_0")
+        , m_ymsnd_1(*this, "ym2149_1")
+        , m_vdp(*this, "tms9918")
     {
     }
 
@@ -54,31 +50,21 @@ namespace
     required_device<ym2149_device> m_ymsnd_1;
     required_device<tms9918_device> m_vdp;
 
-    // memory_share_creator<u8> m_mainmemory;
-
     void main_memmap(address_map &map);
   };
 
   void gtvip_state::gtvip(machine_config &config)
   {
-    G65816(config, m_maincpu, XTAL(3'686'400));
+    G65265(config, m_maincpu, XTAL(3'686'400));
     m_maincpu->set_addrmap(AS_PROGRAM, &gtvip_state::main_memmap);
 
+    // sound chip
     YM2149(config, m_ymsnd_0, XTAL(1'843'200));
     YM2149(config, m_ymsnd_1, XTAL(1'843'200));
     m_ymsnd_0->set_flags(AY8910_SINGLE_OUTPUT);
     m_ymsnd_1->set_flags(AY8910_SINGLE_OUTPUT);
-    // m_ymsnd->set_resistors_load(RES_K(1), 0, 0);
-    // m_ymsnd->port_a_write_callback().set(FUNC(st_state::psg_pa_w));
-    // m_ymsnd->port_b_write_callback().set("cent_data_out", FUNC(output_latch_device::write));
 
-    // example of how atari does it
-    // YM2149(config, m_ymsnd, Y2/16);
-    // m_ymsnd->set_flags(AY8910_SINGLE_OUTPUT);
-    // m_ymsnd->set_resistors_load(RES_K(1), 0, 0);
-    // m_ymsnd->port_a_write_callback().set(FUNC(st_state::psg_pa_w));
-    // m_ymsnd->port_b_write_callback().set("cent_data_out", FUNC(output_latch_device::write));
-
+    // define speaker output
     SPEAKER(config, "speaker", 2).front();
     m_ymsnd_0->add_route(0, "speaker", 1.0, 0);
     m_ymsnd_1->add_route(0, "speaker", 1.0, 1);
@@ -86,14 +72,18 @@ namespace
     // display chip
     // referenced colecovision which uses TMS9928A: src/mame/coleco/coleco.cpp
     // 10.738633MHz clock frequency and 16K vram
-    // not sure about the SCREEN part but just copied it over
     TMS9918(config, m_vdp, XTAL(10'738'635));
-    m_vdp->set_screen("screen");
     m_vdp->set_vram_size(0x4000);
+
+    // define screen output
     SCREEN(config, "screen", SCREEN_TYPE_RASTER);
+    m_vdp->set_screen("screen");
   }
 
   // see MAME docs on memory: https://docs.mamedev.org/techspecs/memory.html
+  //
+  // see RetCom87 docs on memory map:
+  // https://github.com/lantertronics/RetCom87-hardware/wiki/RetCom87-Memory-Map
   void gtvip_state::main_memmap(address_map &map)
   {
     // 32kB SRAM
@@ -116,23 +106,13 @@ namespace
     // sound
     // DF10: Data Send for YM2149 soundchip #1
     // DF11: Register Select for YM2149 soundchip #1
-    // DF12: Data Send for YM2149 soundchip #2
-    // DF13: Register Select for YM2149 soundchip #2
-    // should use DF14 and 15 instead of 12 and 13 for now to match the soundtest binary
-
-    // example of how atari maps their sound chip
-    // map(0xff8800, 0xff8800).after_delay(NAME([](offs_t) { return 1; })).rw(m_ymsnd, FUNC(ay8910_device::data_r), FUNC(ay8910_device::address_w)).mirror(0xfc);
-    // map(0xff8802, 0xff8802).after_delay(NAME([](offs_t) { return 1; })).rw(m_ymsnd, FUNC(ay8910_device::data_r), FUNC(ay8910_device::data_w)).mirror(0xfc);
-    // map(0xff8800, 0xff8800).rw(m_ymsnd, FUNC(ay8910_device::data_r), FUNC(ay8910_device::address_w));
-    // map(0xff8802, 0xff8802).w(m_ymsnd, FUNC(ay8910_device::data_w));
-
-    // example from src/mame/bandai/sv8000.cpp
+    // DF14: Data Send for YM2149 soundchip #2
+    // DF15: Register Select for YM2149 soundchip #2
+    // referenced src/mame/bandai/sv8000.cpp, src/mame/atari/atarist.cpp
     map(0xdf10, 0xdf10).w(m_ymsnd_0, FUNC(ay8910_device::data_w));
     map(0xdf11, 0xdf11).w(m_ymsnd_0, FUNC(ay8910_device::address_w));
     map(0xdf14, 0xdf14).w(m_ymsnd_1, FUNC(ay8910_device::data_w));
     map(0xdf15, 0xdf15).w(m_ymsnd_1, FUNC(ay8910_device::address_w));
-    // map(0xc0, 0xc0).w("ay8910", FUNC(ay8910_device::data_w));
-    // map(0xc1, 0xc1).w("ay8910", FUNC(ay8910_device::address_w));
 
     // controllers
     // DF00-DF03: Controller 1 through 4 inputs (aliased to DF04-DF07)
